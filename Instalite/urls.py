5f4144d0464cae0485c41722d1cc6ace98301796
Yujiao-Chen <13006931+cyj1960@users.noreply.github.com>
2019-11-16 22:22:18 -0500

    display static page

--- Instalite/urls.py
+++ Instalite/urls.py
@@ -13,10 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.urls import include, path
+from Instalite.views import TestView
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('Instalite/', include('Instalite.urls'))
+    path('', TestView.as_view(), name='test'),
 ]
